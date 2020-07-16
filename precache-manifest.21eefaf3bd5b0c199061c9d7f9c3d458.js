c24b609755ad2044e715519514201d70d4dfd3a7
ilan1540 <ilanbarlev@gmail.com>
2020-07-16 13:22:44 +0300

    Updates

---
+++ precache-manifest.21eefaf3bd5b0c199061c9d7f9c3d458.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b11e93c2addaa30bdc88e791bb02b29",
+    "url": "/madd/index.html"
+  },
+  {
+    "revision": "c926d135842637e5000d",
+    "url": "/madd/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "63a255bdc73a31d7fc2d",
+    "url": "/madd/static/js/2.5ac9deef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/madd/static/js/2.5ac9deef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c926d135842637e5000d",
+    "url": "/madd/static/js/main.73ec1b22.chunk.js"
+  },
+  {
+    "revision": "e4d4b6bef80f6c164675",
+    "url": "/madd/static/js/runtime-main.742d3929.js"
+  }
+]);
