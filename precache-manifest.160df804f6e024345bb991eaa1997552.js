114538fe03c2be2b8c628f03fe158b809f5e8858
ilan1540 <ilanbarlev@gmail.com>
2020-08-01 15:52:03 +0300

    Updates

---
+++ precache-manifest.160df804f6e024345bb991eaa1997552.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fabaf2a9f3b947e42e59e76188bb93bb",
+    "url": "/isrelMadd/index.html"
+  },
+  {
+    "revision": "ed1785fe2e5bd6b9d910",
+    "url": "/isrelMadd/static/css/main.8af42587.chunk.css"
+  },
+  {
+    "revision": "7f53402a22d498cd20cf",
+    "url": "/isrelMadd/static/js/2.034504c9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/isrelMadd/static/js/2.034504c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed1785fe2e5bd6b9d910",
+    "url": "/isrelMadd/static/js/main.05301bc1.chunk.js"
+  },
+  {
+    "revision": "03aa0d4c04676101cfdd",
+    "url": "/isrelMadd/static/js/runtime-main.583b4f8d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/madd/precache-manifest.ccf17a373508a0c1fee9f0678179d7d6.js"
+  "/isrelMadd/precache-manifest.160df804f6e024345bb991eaa1997552.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/madd/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/isrelMadd/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
